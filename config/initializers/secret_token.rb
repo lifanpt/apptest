c622e1b073652f4599aad23c2ca1797dc9f7c10c
fan_li_new <fan_li_new@fan-li-no-MacBook-Pro.local>
2016-10-20 11:11:24 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apptest::Application.config.secret_token = '21d8eebaa5245f50a27413605618ac1683ab0284f50a012eeab5bed52e6dd9960377c11b9279febb77703eacdf37cc71656eaf5ea310de31ad60e2b7251fb17b'
